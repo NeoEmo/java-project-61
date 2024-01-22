7c86d3710d304609ff828122c375e5500ff5287a
NeoEmo <Ochenash14@gmail.com>
2024-01-22 19:19:04 +0300

    added Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import hexlet.code.App;
+
+import java.util.Scanner;
+
+class Cli {
+    public static void getByName() {
+        Scanner name = new Scanner(System.in);
+        String userName;
+        System.out.print("May I have your name ");
+
+        // Здесь мог быть цикл if, который проверял бы наличие имени в словаре имён, но его нет.
+        userName = name.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        name.close();
+    }
+}
