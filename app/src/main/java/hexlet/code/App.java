4c7cdaed962c5dc9006ea0750ec6e5c09e9b5b09
NeoEmo <Ochenash14@gmail.com>
2024-01-22 19:20:06 +0300

    hotfix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,17 +2,11 @@ package hexlet.code;
 
 import java.util.Scanner;
 
+import static hexlet.code.Cli.getByName;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-
-        Scanner name = new Scanner(System.in);
-        String userName;
-        System.out.print("May I have your name " );
-
-// Здесь мог быть цикл if, который проверял по словарю наличие имени, но его нет.
-        userName = name.nextLine();
-        System.out.println("Hello, " + userName);
-        name.close();
+        getByName();
     }
 }
