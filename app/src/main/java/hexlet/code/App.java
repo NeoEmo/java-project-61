005baa5148982202530ac977b5e59bbc1ddc8e72
NeoEmo <Ochenash14@gmail.com>
2024-01-24 21:23:45 +0300

    Update App.java 1)added Menu Brain Games on main 2)code on method main replaced on method getGreet

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 import static hexlet.code.Cli.getByName;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        Scanner gameNumber = new Scanner(System.in);
+        int gameNum;
+        System.out.print("Your choice: ");
+        gameNum = gameNumber.nextInt();
+        System.out.println(gameNum);
+        if (gameNum == 1) {
+            App.getGreet();
+        }
+        gameNumber.close();
+    }
+    public  static void getGreet() {
         System.out.println("Welcome to the Brain Games!");
         getByName();
     }
