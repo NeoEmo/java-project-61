54d4ed063e17013449c0c590c897b39c1ddfd420
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-05-13 22:17:02 +0300

    Update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,20 +1,17 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public final class Cli {
     public static String userName;
 
     private Cli() { }
 
     public static void greet() {
-        Scanner scanner = new Scanner(System.in);
         String greet = """
                 Welcome to the Brain Games!
                 May I have your name?\s""";
 
         System.out.print(greet);
-        String name = scanner.nextLine();
+        String name = App.SCANNER.nextLine();
         userName = name;
         System.out.println("Hello, " + name + "!");
     }
